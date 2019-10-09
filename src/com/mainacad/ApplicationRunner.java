74b7cf6c8992ede2f63a3dc35e189cd54d7e40e7
riaskay27 <riaskay2708@ukr.net>
2019-10-09 16:19:43 +0300

    Add min Shape among objects

--- src/com/mainacad/ApplicationRunner.java
+++ src/com/mainacad/ApplicationRunner.java
@@ -9,27 +9,57 @@ public class ApplicationRunner {
 
 
     public static void main(String[] args) {
-      Shape triangle=new Triangle();
-      Shape circle=new Circle();
-      Shape square=new Square();
-
-      Square square10=new Square();
-      Circle squareCircle=new Circle();
-        Triangle squareTriangle= new Triangle();
-        squareTriangle.setHeight1(3.0);
-        squareTriangle.setSide1(7.0);
-      squareCircle.setRadius(10.0);
-      square10.setSide(10.0);
-        System.out.println("Square with "+
-                "radius " + squareCircle.getRadius()+
-                " has area "+ squareCircle.getArea());
-        System.out.println("Square with "+
-                "side " + square10.getSide()+
-                " has area "+ square10.getArea());
-        System.out.println("Square with "+
-                "height " + squareTriangle.getHeight1()+
-                " and side "+ squareTriangle.getSide1() +
-                " has area "+ squareTriangle.getArea());
+      Triangle triangle=new Triangle();
+      triangle.setSide1(15);
+      triangle.setHeight1(10);
+      Circle circle=new Circle();
+      circle.setRadius(12);
+      Square square=new Square();
+      square.setSide(28);
+      Shape maxShape;
+      if(circle.getArea()>square.getArea()){
+        maxShape=circle;
+      }else{
+        maxShape=square;
+      }
+      if(maxShape.getArea()<triangle.getArea()){
+        maxShape=triangle;
+      }
+      System.out.println("Max shape is "+maxShape.getClass().getSimpleName()+" has area "+maxShape.getArea());
+      Triangle triangle1=new Triangle();
+      triangle1.setSide1(15);
+      triangle1.setHeight1(10);
+      Circle circle1=new Circle();
+      circle1.setRadius(12);
+      Square square1=new Square();
+      square1.setSide(28);
+      Shape minShape;
+      if(circle1.getArea()<square1.getArea()){
+        minShape=circle1;
+      }else{
+        minShape=square1;
+      }
+      if(minShape.getArea()<triangle1.getArea()){
+        minShape=triangle1;
+      }
+      System.out.println("Min shape is "+minShape.getClass().getSimpleName()+" has area "+minShape.getArea());
+//      Square square10=new Square();
+//      Circle squareCircle=new Circle();
+//        Triangle squareTriangle= new Triangle();
+//        squareTriangle.setHeight1(3.0);
+//        squareTriangle.setSide1(7.0);
+//      squareCircle.setRadius(10.0);
+//      square10.setSide(10.0);
+//        System.out.println("Square with "+
+//                "radius " + squareCircle.getRadius()+
+//                " has area "+ squareCircle.getArea());
+//        System.out.println("Square with "+
+//                "side " + square10.getSide()+
+//                " has area "+ square10.getArea());
+//        System.out.println("Square with "+
+//                "height " + squareTriangle.getHeight1()+
+//                " and side "+ squareTriangle.getSide1() +
+//                " has area "+ squareTriangle.getArea());
 
     }
 }
